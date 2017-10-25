3e72d4c42462c26d0af90d0c0a63f5d4888173d8
Mariley20 <mariley2018@gmail.com>
2017-10-25 14:36:23 -0500

    formulario

--- src/App.js
+++ src/App.js
@@ -6,9 +6,20 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <form>
+        <div class="form-group">
+          <label for="email">Email address:</label>
+          <input type="email" class="form-control" id="email">
+        </div>
+        <div class="form-group">
+          <label for="pwd">Password:</label>
+          <input type="password" class="form-control" id="pwd">
+        </div>
+        <div class="checkbox">
+          <label><input type="checkbox"> Remember me</label>
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
     );
   }
 }
