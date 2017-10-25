a7dcafb913e0fc7139eb46837bb3dc342c54685f
Mariley20 <mariley2018@gmail.com>
2017-10-25 14:33:33 -0500

    app and actions js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from "redux-zero/react";
+import { addComment } from "./actions";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </div>
     );
   }
 }
 
-export default App;
+const mapToProps = ({commentary, selectedComment}) =>({commentary, selectedComment});
+
+export default connect(mapToProps)(App);
---
+++ src/actions.js
@@ -0,0 +1,4 @@
+import store from "./store";
+export const addComment = (comment) => {
+    console.log("add");
+};
