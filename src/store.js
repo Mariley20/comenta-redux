f5fcf4b7a5c1485a60b0a75eeaf446d747174f63
Mariley20 <mariley2018@gmail.com>
2017-10-25 14:28:37 -0500

    store.js

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import createStore from "redux-zero";
+let commentary = [];
+const initialState = {
+    commentary: commentary,
+    selectedComment: 0
+};
+
+const store = createStore(initialState);
+export default store;
