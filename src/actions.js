4220e24cfb461e6c0c7f64d5861b776f46e0fe31
Mariley20 <mariley2018@gmail.com>
2017-10-26 07:47:11 -0500

    conditional of comments undefined

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,7 @@
 import store from "./store";
 export const addComment = (name, comment) => {
-    console.log("name "+name+"-commet-"+comment);
+    // console.log("name "+name+"-commet-"+comment);
+    if(name != "" && comment != ""){
     const addCommentary = [...store.getState().commentary, {
 		id : store.getState().commentary.length + 1,
 		name: name,
@@ -9,7 +10,9 @@ export const addComment = (name, comment) => {
 
 	store.setState({
 		commentary: addCommentary
-	})
+    })
+}
+console.log('no agrego nada');
 };
 export const removeComment = (index) => {
 	const addCommentary =  store.getState().commentary.filter( (item, idx) => idx != index );
