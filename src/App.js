3b00bb1146ac97466f996a7fec359c544a91fa75
Mariley20 <mariley2018@gmail.com>
2017-10-26 00:58:11 -0500

    comment

--- src/App.js
+++ src/App.js
@@ -3,20 +3,39 @@ import { connect } from "redux-zero/react";
 import { addComment } from "./actions";
 import './App.css';
 
+const ListComments = ({commentary}) => {
+    let list = commentary.map((element, index) => {
+      return (
+        <div key={index}>
+            <p>{element.name}</p>
+            <p>{element.comment}</p>
+        </div>
+      )
+    });
+    return (<div>{list}</div>)
+}
+
 const App = ({commentary, selectedComment}) => {
+  const onSubmit = (e) => {
+		e.preventDefault();
+		console.log ( 'this..', this);//con truco, es el connect el this.
+		addComment(this.userName.value, this.comment.value )
+  }
+  console.log(commentary)
     return (
       <div className="container">
-      <form>
+      <form onSubmit = {onSubmit}>
         <div className="form-group">
           <label >Name User: </label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" ref={(e) => this.userName = e} />
         </div>
         <div className="form-group">
           <label>Comment:</label>
-          <textarea className="form-control textarea"> </textarea>
+          <textarea className="form-control textarea" ref={(e) => this.comment = e} > </textarea>
         </div>
         <button type="submit" className="btn btn-default">Submit</button>
       </form>
+          <ListComments commentary={commentary} />
       </div>
     );
 }
--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,13 @@
 import store from "./store";
-export const addComment = (comment) => {
-    console.log("add");
+export const addComment = (name, comment) => {
+    console.log("name "+name+"-commet-"+comment);
+    const addCommentary = [...store.getState().commentary, {
+		id : store.getState().commentary.length + 1,
+		name: name,
+		comment: comment
+	}];
+
+	store.setState({
+		commentary: addCommentary
+	})
 };
